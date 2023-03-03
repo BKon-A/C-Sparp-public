4652534924d84397209a1a287bbbe2cba28377a5
BKon-A <bogdankonovalenko291@gmail.com>
2023-03-03 13:56:04 +0200

    Update

---
+++ ConsoleApp2/OOP_Lab 12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lab_12
+{
+    internal class Program
+    {
+        public class NormalVector
+        {
+            private double coordinateX;
+            private double coordinateY;
+
+            NormalVector(double coordinateX, double coordinateY)
+            { 
+                this.coordinateX = coordinateX;
+                this.coordinateY = coordinateY;
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
